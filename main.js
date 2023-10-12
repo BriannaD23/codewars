f4f9a0c7d9509b9a5d1324c7fe229e25df34612d
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-10-12 11:51:50 -0700

    2 other problems!

--- main.js
+++ main.js
@@ -3,3 +3,34 @@
 function solution(str){
   return str.split('').reverse().join('')
 };
+
+//Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+function spinWords(string){
+    let words = string.split(' ');
+    
+    for(let i=0; i < words.length; i++){
+      if(words[i].length >= 5)
+      words[i] = words[i].split('').reverse().join('');
+    }
+     
+    let reversedString = words.join(' ');
+    return reversedString;
+}
+  
+const phrase = "How can super woman fly south of the mountain";
+const result = spinWords(phrase);
+  
+console.log(result)
+
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if( number % 2 === 0){
+     return 'Even';
+    } else{
+      return 'Odd';
+    }
+    
+};
