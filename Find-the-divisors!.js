8470e65b1223213866beb71406643adc5246e27f
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-10-16 15:32:29 -0700

    code-wars solution

---
+++ Find-the-divisors!.js
@@ -0,0 +1,20 @@
+function divisors(integer) {
+    let divisors =[];
+     
+    for(let i = 2; i < integer; i++){
+      if(integer % i === 0){
+        divisors.push(i);
+      }
+    }
+      
+      
+    if( divisors.length === 0){
+      return `${integer} is prime`;
+    }
+
+    return divisors;
+    
+}
+
+// you ranked up to 7kyu
+  
