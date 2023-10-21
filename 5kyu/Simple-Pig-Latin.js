71e01f51079bebfc3dece1180f2a7ed823ab6dcb
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-10-21 11:55:00 -0700

    fixing code

--- 5kyu/Simple-Pig-Latin.js
+++ 5kyu/Simple-Pig-Latin.js
@@ -17,4 +17,13 @@ function pigIt(str) {
     return pigLatinWords.join(' ');
 }
 
+// another solution
+
+// function pigIt(str) {
+//     var arrayWord = str.split(' ');
+//     return arrayWord.map(function(word) {
+//       var firstLetter = word.charAt(0);
+//       return word.slice(1) + firstLetter + 'ay';
+//     }).join(' ');
+// }
 
