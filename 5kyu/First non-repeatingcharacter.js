c55841bba67542e015a85eeb5a5dd039c5db239f
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-10-23 23:56:39 -0700

    code wars solution of the day

---
+++ 5kyu/First non-repeatingcharacter.js
@@ -0,0 +1,25 @@
+function firstNonRepeatingLetter(s) {
+    // Create an object to store the count of each character
+    const charCount = {};
+  
+    // Convert the string to lowercase for case-insensitivity
+    const lowerCaseString = s.toLowerCase();
+  
+    // Count the occurrences of each character
+    for (let i = 0; i < lowerCaseString.length; i++) {
+      const char = lowerCaseString[i];
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+  
+    // Find the first non-repeating character and its case
+    for (let i = 0; i < s.length; i++) {
+      const char = s[i];
+      if (charCount[lowerCaseString[i]] === 1) {
+        return char;
+      }
+    }
+  
+    // If all characters are repeating or the string is empty, return an empty string
+    return "";
+  }
+  
