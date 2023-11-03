6b363e0a7a8fab8214b06b2d7423ba3a3a2c3553
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-11-02 23:49:57 -0700

    codewar solution

---
+++ 7yku/ Categorize-New-Member.js
@@ -0,0 +1,12 @@
+function openOrSenior(data){
+  
+    return data.map(([age, handicap]) => {
+    
+      if( age >= 55 && handicap > 7){
+        return 'Senior'
+      }else {
+        return 'Open'
+      };
+      
+    });
+}
