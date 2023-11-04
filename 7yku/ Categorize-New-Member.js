9f4c80249dbaf86ece0a6da826f67597401ee1fd
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-11-03 23:51:32 -0700

    second solution

--- 7yku/ Categorize-New-Member.js
+++ 7yku/ Categorize-New-Member.js
@@ -10,3 +10,10 @@ function openOrSenior(data){
       
     });
 }
+
+
+// function openOrSenior(data){
+//     return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+    
+// }
+  
