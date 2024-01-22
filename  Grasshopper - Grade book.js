5890b04b5571da7de6813f67fbcdf5b38c86bc49
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-01-21 23:46:52 -0800

    retouching on solution

---  Grasshopper - Grade book.js
+++  Grasshopper - Grade book.js
@@ -15,4 +15,15 @@ function getGrade(score1, score2, score3) {
       return 'F';
     }
   }
+
+
+/*
+amother solution
+
+function getGrade (s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+}
+
+*/
   
