8aab41afaa8ae0c0e8283ea706cf70e46f2b40a7
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-02-05 20:30:29 -0800

    second solution

--- 5kyu/Simple-Pig-Latin.js
+++ 5kyu/Simple-Pig-Latin.js
@@ -1,20 +1,11 @@
 //Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
 function pigIt(str) {
-    const words = str.split(' ');
-  
-    const pigLatinWords = words.map(word => {
-      if (/^[a-zA-Z]+$/.test(word)) { // Check if the word contains only letters
-        const firstLetter = word[0];
-        const restOfWord = word.slice(1);
-        return restOfWord + firstLetter + 'ay';
-      } else {
-        // If the word contains non-alphabetic characters, leave it as is
-        return word;
-      }
-    });
-  
-    return pigLatinWords.join(' ');
+    var arrayWord = str.split(' ');
+    return arrayWord.map(function(word) {
+      var firstLetter = word.charAt(0);
+      return word.slice(1) + firstLetter + 'ay';
+    }).join(' ');
 }
 
 // another solution
