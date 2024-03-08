7faad0ff7b014aa69991d5d2ed5e4c87ad882afb
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-03-07 23:51:15 -0800

    a second solution for this probem

--- 7yku/DisemvowelTrolls.js
+++ 7yku/DisemvowelTrolls.js
@@ -6,4 +6,23 @@ function disemvowel(str) {
     
     
 }
+/*
+another solution
+
+function disemvowel(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (!vowels.includes(str[i].toLowerCase())) {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+
+
+*/
   
