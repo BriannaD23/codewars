b9ef89d944347d2a98c6d51244498864ed5757b5
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-07-19 23:53:17 -0400

    8kyu simple solution

---
+++ 8kyu/Square(n)Sum.js
@@ -0,0 +1,13 @@
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+
+function squareSum(numbers){
+    let sum = 0;
+    
+    for(let i =0; i < numbers.length; i++){
+      let squaredNum = Math.pow(numbers[i], 2);
+      sum += squaredNum;
+    }
+    
+    return sum;
+}
