7ca7844624c4c2e56d99e6a58c24a6936a71132f
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-10-01 10:36:16 -0400

    8kyu solution

---
+++ 8kyu/Count-of-positives.js
@@ -0,0 +1,17 @@
+function countPositivesSumNegatives(input) {
+    if(!input || input.length === 0) return [];
+    
+    let positiveCount = 0;
+    let sumNegative = 0;
+    
+    input.forEach(number =>{
+      if(number > 0){
+        positiveCount += 1;
+      }else if(number < 0){
+        sumNegative += number;
+      }
+      
+    });
+    
+    return [positiveCount, sumNegative]
+}
