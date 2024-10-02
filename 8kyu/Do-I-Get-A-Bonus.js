916c7dadee687409fe0d8c2df75384875577b558
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-10-01 23:57:46 -0400

    Do I get a bonus 8 kyu solution

---
+++ 8kyu/Do-I-Get-A-Bonus.js
@@ -0,0 +1,13 @@
+function bonusTime(salary, bonus) {
+    // function takes in two arguements
+    //salary interger
+    //bonus is a bool
+      
+      if (bonus === true) {
+        return `£${salary * 10}`;
+      } else {
+        return `£${salary}`;
+      }
+}
+    
+      
