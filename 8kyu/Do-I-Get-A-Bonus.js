0a5f0a36f51516daaaf60da44da0315324bc8fab
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-10-03 23:53:40 -0400

    adding comment to solution

--- 8kyu/Do-I-Get-A-Bonus.js
+++ 8kyu/Do-I-Get-A-Bonus.js
@@ -2,6 +2,7 @@ function bonusTime(salary, bonus) {
     // function takes in two arguements
     //salary interger
     //bonus is a bool
+    //£ this is punds we add $ to make it US currency
       
       if (bonus === true) {
         return `£${salary * 10}`;
