5d954f5159b62517e7a6e2a165b7d3de0a62ebb0
BreezytheCoder <98433092+BriannaD23@users.noreply.github.com>
2025-03-04 19:11:55 -0500

    Update solution - CodeHub

--- 8-kyu/tip-calculator/tip-calculator.js
+++ 8-kyu/tip-calculator/tip-calculator.js
@@ -1,22 +1,15 @@
 function calculateTip(amount, rating) {
-    let lowerCaseRating = rating.toLowerCase();
+  let tips = {
+    'terrible' : 0,
+      'poor': .05,
+       'good' : .1,
+        'great':.15,
+        'excellent':.2,
+  }
 ​
-    let tip;
+  rating = rating.toLowerCase();
 ​
-    if (lowerCaseRating === "excellent") {
-        tip = Math.ceil(amount * 0.20); 
-    } else if (lowerCaseRating === "great") {
-        tip = Math.ceil(amount * 0.15); 
-    } else if (lowerCaseRating === "good") {
-        tip = Math.ceil(amount * 0.10); 
-    } else if (lowerCaseRating === "poor") {
-        tip = Math.ceil(amount * 0.05); 
-    } else if (lowerCaseRating === "terrible") {
-        tip = 0;
-    } else {
-        return "Rating not recognised"; 
-    }
+  return rating in tips? Math.ceil(amount * tips[rating]) : "Rating not recognised"
 ​
-    return tip;
+​
 }
-​
