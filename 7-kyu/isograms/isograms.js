b1aba421e57d1d3b81dcb082207475a41bcf69c9
BreezytheCoder <98433092+BriannaD23@users.noreply.github.com>
2025-03-30 22:11:59 -0400

    Update solution - CodeHub

--- 7-kyu/isograms/isograms.js
+++ 7-kyu/isograms/isograms.js
@@ -1,14 +1,6 @@
 function isIsogram(str){
-  // so basically we have an isogram that has no non repeating numbers
-​
-  if( str ===''){
-    return true;
-  } 
-  if(!/^[a-zA-Z]+$/.test(str)){
-    return false; 
-  }
-​
-  let word = new Set(str.toLowerCase());
-​
-  return word.size === str.length
+  
+ return new Set(str.toLowerCase()).size == str.length
 }
+​
+console.log(isIsogram('mo0se'))
