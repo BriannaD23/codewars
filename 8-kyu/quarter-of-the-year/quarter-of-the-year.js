b72679d595c7b120e7979369a3e4acea750c6ed6
BreezytheCoder <98433092+BriannaD23@users.noreply.github.com>
2025-04-08 11:13:13 -0400

    Add solution - CodeHub

---
+++ 8-kyu/quarter-of-the-year/quarter-of-the-year.js
@@ -0,0 +1,20 @@
+const quarterOf = (month) => {
+  // we have three quarters
+  // each month represents an interger
+  // each quarter is split up by 4
+  // start off with an if statement is month is <= to the quarter of months then we will r
+  // return
+  
+  if( month <= 3){
+    return 1
+  }else if( month <= 6 ){
+    return 2
+  }else if( month <= 9){
+    return 3
+  }else if( month <= 12){
+    return 4
+  }
+​
+   
+  
+}
