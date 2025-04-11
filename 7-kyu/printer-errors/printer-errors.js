e5b0ad2101eadb14e954f3f715b63d095487d995
BreezytheCoder <98433092+BriannaD23@users.noreply.github.com>
2025-04-11 14:10:56 -0400

    Add solution - CodeHub

---
+++ 7-kyu/printer-errors/printer-errors.js
@@ -0,0 +1,22 @@
+function printerError(s) {
+    let errorCount = 0;
+​
+    for(let char of s){
+       if(!/[a-m]$/.test(char)){
+           errorCount++
+       }
+     }
+    return `${errorCount}/${s.length}`
+}
+​
+// function printerError(s) {
+//   let errorCount = 0;
+​
+//   for (let char of s) {
+//     if (!/^[a-m]$/.test(char)) {
+//       errorCount++;
+//     }
+//   }
+​
+//   return `${errorCount}/${s.length}`;
+// }
