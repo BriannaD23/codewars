c5d7dc1ccee0c54f74cd2e5d82bcc1ea88197878
BreezytheCoder <98433092+BriannaD23@users.noreply.github.com>
2025-05-06 23:10:48 -0400

    Update solution - CodeHub

--- 7-kyu/sum-it-continuously/sum-it-continuously.js
+++ 7-kyu/sum-it-continuously/sum-it-continuously.js
@@ -1,11 +1,16 @@
 function add(arr) {
-// we need to sum all elements
-  let cumalativeSum = []
-  let runningTotal = 0;
-  
-  for(let nums of arr){
-    runningTotal += nums;
-    cumalativeSum.push(runningTotal)
-  }
-  return cumalativeSum
+// we have an array
+// the right array is the product of teh left array being sumed all together
+// have two variable one to hold the array of numbers and the other to be the running result
+let sumArray = []; 
+let runningTotal = 0;
+​
+for(let num of arr){
+  runningTotal += num
+  sumArray.push(runningTotal)
+ 
+​
+}
+ return sumArray
+​
 }
